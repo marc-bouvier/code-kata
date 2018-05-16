dd62bc982467ca467acd96c09a9babfb82c7b1bc
marc-bouvier <m.bouvier.dev@gmail.com>
2018-05-16 16:39:40 +0200

    stage 2 naif

--- README.md
+++ README.md
@@ -61,7 +61,6 @@ Buzz
 
  * A number is fizz if it is divisible by 3 or if it has a 3 in it
  * A number is buzz if it is divisible by 5 or if it has a 5 in it
-Explication du Kata + liens
 
 ### Approche utilisée
 Java, Junit 5 , maven, tests paramétrés
--- src/main/java/fr/bouvier/marc/kata/FizzBuzz.java
+++ src/main/java/fr/bouvier/marc/kata/FizzBuzz.java
@@ -1,5 +1,8 @@
 package fr.bouvier.marc.kata;
 
+// Fizz => nombre divisible par 3
+// Buzz => nombre divisible par 5
+// sinon => nombre
 public final class FizzBuzz {
   public static String parse(int number) {
     String result = "";
@@ -7,6 +10,24 @@ public final class FizzBuzz {
       result += "Fizz";
     }
     if (number % 5 == 0) {
+      result += "Buzz";
+    }
+    if (result == "") {
+      result += String.valueOf(number);
+    }
+    return result;
+
+  }
+
+  // Fizz => nombre divisible par 3 ou contient 3
+  // Buzz => nombre divisible par 5 ou contient 5
+  // sinon => nombre
+  public static String parse2(int number) {
+    String result = "";
+    if (number % 3 == 0 || String.valueOf(number).contains("3")) {
+      result += "Fizz";
+    }
+    if (number % 5 == 0 || String.valueOf(number).contains("5")) {
       result += "Buzz";
     }
     if (result == "") {
