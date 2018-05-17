c0fecae643fe02c79d44839eadade5eb68851d46
marc-bouvier <m.bouvier.dev@gmail.com>
2018-05-17 12:00:36 +0200

    Main with stage 1 and 2

--- src/main/java/fr/bouvier/marc/kata/Main.java
+++ src/main/java/fr/bouvier/marc/kata/Main.java
@@ -1,11 +1,14 @@
 package fr.bouvier.marc.kata;
 
-public class Main{
-  public static void main(String [] args){
-    System.out.println("Hello World!");
-    // Divisible par 3 => Fizz!
-    // Divisible par 5 => Buzz!
-    // Divisible par 3 et 5 => FizzBuzz!
-    // Sinon => afficher le nombre
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("\n==== Stage 1 ====\n");
+    for (int i = 0; i < 100; i++) {
+      System.out.println(FizzBuzz.parse(i));
+    }
+    System.out.println("\n==== Stage 2 ====\n");
+    for (int i = 0; i < 100; i++) {
+      System.out.println(FizzBuzz.parse(i));
+    }
   }
 }
