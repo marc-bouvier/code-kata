65606223cd9b7d49850583ad0feabdac835ec092
marc-bouvier <m.bouvier.dev@gmail.com>
2018-05-17 18:46:29 +0200

    liste des 100 premiers fizzbuzz

--- src/Main.elm
+++ src/Main.elm
@@ -8,22 +8,11 @@ import Html exposing (li, text, ul)
 -- fonction pour
 
 
+numbers =
+    List.range 1 100
+
+
 main : Html.Html msg
 main =
-    ul []
-        [ li [] [ text <| fizzbuzz 1 ]
-        , li [] [ text <| fizzbuzz 2 ]
-        , li [] [ text <| fizzbuzz 3 ]
-        , li [] [ text <| fizzbuzz 4 ]
-        , li [] [ text <| fizzbuzz 5 ]
-        , li [] [ text <| fizzbuzz 6 ]
-        , li [] [ text <| fizzbuzz 7 ]
-        , li [] [ text <| fizzbuzz 8 ]
-        , li [] [ text <| fizzbuzz 9 ]
-        , li [] [ text <| fizzbuzz 10 ]
-        , li [] [ text <| fizzbuzz 11 ]
-        , li [] [ text <| fizzbuzz 12 ]
-        , li [] [ text <| fizzbuzz 13 ]
-        , li [] [ text <| fizzbuzz 14 ]
-        , li [] [ text <| fizzbuzz 15 ]
-        ]
+    ul [] <|
+        List.map (\number -> li [] [ text <| fizzbuzz number ]) numbers
