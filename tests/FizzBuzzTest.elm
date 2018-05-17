700bd3d21bce6dc67ac030ea2246e20fa514c5a7
marc-bouvier <m.bouvier.dev@gmail.com>
2018-05-17 14:29:28 +0200

    update test

--- tests/FizzBuzzTest.elm
+++ tests/FizzBuzzTest.elm
@@ -9,7 +9,7 @@ suite : Test
 suite =
     describe "FizzBuzz"
         -- Nest as many descriptions as you like.
-        [ test "fizzbuzz 1 " <|
+        [ test "fizzbuzz 1" <|
             \_ ->
                 let
                     fizzbuzz1 = fizzbuzz 1
